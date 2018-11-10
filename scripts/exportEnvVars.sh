351f6437ca5467d2f4880ac31c98842c5ee99eba
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-11-10 18:43:56 +0530

    Removing env variables form source control

--- scripts/exportEnvVars.sh
+++
@@ -1,8 +0,0 @@
-export ONPREM_SUFFIX=na39.openshift.opentlc.com
-export AWS_SUFFIX=example.com
-export GCP_SUFFIX=35.244.0.56.nip.io
-export AZURE_SUFFIX=104.211.231.1.nip.io
-echo $ONPREM_SUFFIX
-echo $AWS_SUFFIX
-echo $GCP_SUFFIX
-echo $AZURE_SUFFIX

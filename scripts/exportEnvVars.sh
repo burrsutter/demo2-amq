50b2b957c2bf26ad602321aec6241aef535217f7
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-11-10 12:24:08 +0530

    first workable version

--- scripts/exportEnvVars.sh
+++ scripts/exportEnvVars.sh
@@ -1,7 +1,7 @@
-export ONPREM_SUFFIX=$(minishift ip).nip.io
-export AWS_SUFFIX=amazon.com
-export GCP_SUFFIX=35.244.14.24.nip.io
-export AZURE_SUFFIX=azure.com
+export ONPREM_SUFFIX=na39.openshift.opentlc.com
+export AWS_SUFFIX=example.com
+export GCP_SUFFIX=35.244.0.56.nip.io
+export AZURE_SUFFIX=104.211.231.1.nip.io
 echo $ONPREM_SUFFIX
 echo $AWS_SUFFIX
 echo $GCP_SUFFIX
